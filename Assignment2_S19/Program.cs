aefe1a3e6f6914b9be5f4a868324f5849c9b5d5f
pratik <pratik.utpat@gmail.com>
2019-02-10 22:08:42 -0500

    updated file

--- Assignment2_S19/Program.cs
+++ Assignment2_S19/Program.cs
@@ -34,13 +34,13 @@ namespace Assignment2_S19
 
             // grading students
             Console.WriteLine("\n\nGrading students");
-            int[] grades = { 73, 67, 38, 33 };          
+            int[] grades = { 73, 67, 38, 33 };
             int[] r3 = gradingStudents(grades);
             displayArray(r3);
 
             // find the median
             Console.WriteLine("\n\nFind the median");
-            int[] arr2 = { 0, 1, 2, 4, 6, 5, 3};
+            int[] arr2 = { 0, 1, 2, 4, 6, 5, 3 };
             Console.WriteLine(findMedian(arr2));
 
             // closest numbers
@@ -48,17 +48,18 @@ namespace Assignment2_S19
             //int[] arr3 = { 5, 4, 3, 2 };
             int[] arr3 = { 6, -5, 15, 25, 71, 63 };
             int[] r4 = closestNumbers(arr3);
-            displayArray(r4);            
+            displayArray(r4);
 
             // Day of programmer
             Console.WriteLine("\n\nDay of Programmer");
             int year = 2017;
             Console.WriteLine(dayOfProgrammer(year));
+            Console.ReadKey();
         }
 
-        static void displayArray(int []arr) {       // method used to display content of array.
+        static void displayArray(int[] arr) {       // method used to display content of array.
             Console.WriteLine();
-            foreach(int n in arr) {
+            foreach (int n in arr) {
                 Console.Write(n + " ");
             }
         }
@@ -93,7 +94,7 @@ namespace Assignment2_S19
         static int[] sortArr(int[] arr) // We are sorting our array through this general algorithm of sorting.
         {
             int mini; // Here we create a minimum
-            
+
             int n; // here we declare integer n which help me to complete my indexing logic
 
             for (int i = 0; i < arr.Length; i++)
@@ -140,7 +141,7 @@ namespace Assignment2_S19
 
             return i - 1;
         }
-        
+
 
         // Complete the balancedSums function below.
         static string balancedSums(List<int> arr)
@@ -203,31 +204,38 @@ namespace Assignment2_S19
         // Complete the gradingStudents function below.
         static int[] gradingStudents(int[] grades)
         {
-            bool check = true;
-            for (int j = 0; j < grades.Length; j++)     // for loop used for iteration and checking if all the numbers are between 0 to 100
+            try
             {
-                if (grades[j] < 0 || grades[j] > 100) // checking validation - all the grades should be between 0 to 100
+                bool check = true;
+                for (int j = 0; j < grades.Length; j++)     // for loop used for iteration and checking if all the numbers are between 0 to 100
                 {
-                    check = false;
-                }
-            }
-
-            if (check)      // in this block, if all the numbers are between 0 to 100 then we will call method and start applying the round off rules
-            {
-                for (int i = 0; i < grades.Length; i++)     // for loop used for iteration to get every number of array one by one
-                {
-                    if (grades[i] >= 38)    // checking if the number is greater than/equal to 38 because if the value of grade is less than 38, no rounding occurs because the result will still be a failing grade 
+                    if (grades[j] < 0 || grades[j] > 100) // checking validation - all the grades should be between 0 to 100
                     {
-                        if ((5 - grades[i] % 5) < 3)        // checking whether the difference between the grade and the next multiple of 5 is less than 3
+                        check = false;
+                    }
+                }
+
+                if (check)      // in this block, if all the numbers are between 0 to 100 then we will call method and start applying the round off rules
+                {
+                    for (int i = 0; i < grades.Length; i++)     // for loop used for iteration to get every number of array one by one
+                    {
+                        if (grades[i] >= 38)    // checking if the number is greater than/equal to 38 because if the value of grade is less than 38, no rounding occurs because the result will still be a failing grade 
                         {
-                            grades[i] = (grades[i] + (5 - grades[i] % 5));      // rounding off the number as per the given rule
+                            if ((5 - grades[i] % 5) < 3)        // checking whether the difference between the grade and the next multiple of 5 is less than 3
+                            {
+                                grades[i] = (grades[i] + (5 - grades[i] % 5));      // rounding off the number as per the given rule
+                            }
                         }
                     }
                 }
+                else
+                {
+                    Console.WriteLine("\nall the grades should be between 0 to 100.");
+                }
             }
-            else
+            catch
             {
-                Console.WriteLine("\nall the grades should be between 0 to 100.");
+                Console.WriteLine("Exception occured while computing gradingStudents()");
             }
             return grades;      // returning updated array which contains rounded off grades
         }
@@ -235,7 +243,7 @@ namespace Assignment2_S19
         // method for sorting array in ascending order
         static int[] SortNumbers(int[] arr)
         {
-            for (int i = 0; i < arr.Length-1; i++)      // loop for iteration
+            for (int i = 0; i < arr.Length - 1; i++)      // loop for iteration
             {
                 for (int j = i + 1; j < arr.Length; j++)    // loop for comparing numbers and swapping
                 {
@@ -281,29 +289,36 @@ namespace Assignment2_S19
         {
             int[] pairsFinal;
             List<int> pairs = new List<int>();         // using list because we are not sure of array size
-            if (arr.Length <= 1)                     // corner case - there should not be only 1 input, we need at least 2 inputs
+            try
             {
-                Console.WriteLine("Provide at least 2 numbers");
-            }
-            else
-            {
-                arr = SortNumbers(arr);             // we will get the sorted array here
-                int min = Math.Abs(arr[1] - arr[0]);    // getting the difference between first 2 elements while initialsing 'min' variable
-
-                for (int i = 2; i < arr.Length ; i++) // this loop is for getting the minimun difference among all the numbers
+                if (arr.Length <= 1)                     // corner case - there should not be only 1 input, we need at least 2 inputs
                 {
-                    int diff = Math.Abs(arr[i] - arr[i - 1]);
-                    if (diff < min)
-                        min = diff;
+                    Console.WriteLine("Provide at least 2 numbers");
                 }
-                for (int i = 1; i < arr.Length; i++)        // here we are looking for the pairs which have the minimum difference that we got in last loop.
+                else
                 {
-                    if ((Math.Abs(arr[i] - arr[i - 1])) == min)
+                    arr = SortNumbers(arr);             // we will get the sorted array here
+                    int min = Math.Abs(arr[1] - arr[0]);    // getting the difference between first 2 elements while initialsing 'min' variable
+
+                    for (int i = 2; i < arr.Length; i++) // this loop is for getting the minimun difference among all the numbers
                     {
-                        pairs.Add(arr[i - 1]);                  // adding pairs to the list
-                        pairs.Add(arr[i]);              
+                        int diff = Math.Abs(arr[i] - arr[i - 1]);
+                        if (diff < min)
+                            min = diff;
+                    }
+                    for (int i = 1; i < arr.Length; i++)        // here we are looking for the pairs which have the minimum difference that we got in last loop.
+                    {
+                        if ((Math.Abs(arr[i] - arr[i - 1])) == min)
+                        {
+                            pairs.Add(arr[i - 1]);                  // adding pairs to the list
+                            pairs.Add(arr[i]);
+                        }
                     }
                 }
+            }
+            catch
+            {
+                Console.WriteLine("Exception occured while computing closestNumbers()");
             }
             pairsFinal = pairs.ToArray();           // converting list to array
             return pairsFinal;
@@ -312,32 +327,42 @@ namespace Assignment2_S19
         // Complete the dayOfProgrammer function below.
         static string dayOfProgrammer(int year)
         {
-            int RegularDays = 243;              // total regular days of month from january to august (non leap year)
-            int ProgDays = 256;
-            int SeptDays = ProgDays - RegularDays;  // programming day in sept. month
+            try
+            {
+                int RegularDays = 243;              // total regular days of month from january to august (non leap year)
+                int ProgDays = 256;
+                int SeptDays = ProgDays - RegularDays;  // programming day in sept. month
 
-            if (year >= 1700 && year <= 1917)    // for julian calender system
-            {
-                if (year % 4 == 0)               // checking for leap year condition
+                if (year >= 1700 && year <= 1917)    // for julian calender system
                 {
-                    SeptDays = SeptDays - 1;
-                }
+                    if (year % 4 == 0)               // checking for leap year condition
+                    {
+                        SeptDays = SeptDays - 1;
+                    }
 
-            }
-            if (year > 1918)       // for gregorian calender system
-            {
-                if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) // checking for leap year condition
-                {
-                    SeptDays = SeptDays - 1;
                 }
+                if (year > 1918)       // for gregorian calender system
+                {
+                    if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) // checking for leap year condition
+                    {
+                        SeptDays = SeptDays - 1;
+                    }
+                }
+                if (year == 1918)        // for transition year
+                {
+                    SeptDays = SeptDays + 14;
+                }
+                return SeptDays + ".09." + year;
+
+                
+                
             }
-            if (year == 1918)        // for transition year
+
+            catch
             {
-                SeptDays = SeptDays + 14;
+                Console.WriteLine("Exception occured while computing closestNumbers()");
             }
-            Console.WriteLine(SeptDays + ".09." + year);
-            Console.ReadKey();
             return "";
         }
     }
-}
+    }
